db567c48003b46aab5c411e9f65f1aa260a453ea
shraddhaa09 <shraddhakhetmalis915@gmail.com>
2026-06-21 21:01:21 +0530

    solution to 23 assignment

---
+++ assignments/assignment_24/2.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+//1:
+/*
+void Pattern(int iRow,int iCol){
+    int i=0,j=0;
+    char ch='A';
+    for(i=1;i<=iRow;i++){
+        for(j=1;j<=iCol;j++){
+            printf("%c\t",ch);
+            ch++;
+        }
+        ch='A' ;
+        printf("\n");
+    }
+}*/
+
+//2:
+
+void Pattern(int iRow,int iCol){
+    int i=0,j=0;
+    char ch='A';
+    char ch1='a';
+    for(i=1;i<=iRow;i++){
+        for(j=1;j<=iCol;j++){
+            if(i%2==0){
+                printf("%c\t",ch1+j-1);
+            }
+            else{
+                printf("%c\t",ch+j-1);
+            }
+        }
+        printf("\n");
+    }
+}
+int main(){
+    int iValue1=0,iValue2=0;
+    printf("Enter number of rows");
+    scanf("%d",&iValue1);
+    printf("Enter number of columns");
+    scanf("%d",&iValue2);
+    Pattern(iValue1,iValue2);
+    return 0;
+}
---
+++ assignments/assignment_24/4.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+//1:
+/*
+void Pattern(int iRow,int iCol){
+    int i=0,j=0;
+    char ch='A';
+    for(i=1;i<=iRow;i++){
+        for(j=1;j<=iCol;j++){
+            printf("%c\t",ch);
+            ch++;
+        }
+        ch='A' ;
+        printf("\n");
+    }
+}*/
+
+//2:
+
+void Pattern(int iRow,int iCol){
+    int i=0,j=0;
+    char ch='A';
+    char ch1='a';
+    for(i=1;i<=iRow;i++){
+        for(j=1;j<=iCol;j++){
+            if(j%2==0){
+                printf("#\t");
+            }
+            else{
+                printf("*\t");
+            }
+        }
+        printf("\n");
+    }
+}
+int main(){
+    int iValue1=0,iValue2=0;
+    printf("Enter number of rows");
+    scanf("%d",&iValue1);
+    printf("Enter number of columns");
+    scanf("%d",&iValue2);
+    Pattern(iValue1,iValue2);
+    return 0;
+}
