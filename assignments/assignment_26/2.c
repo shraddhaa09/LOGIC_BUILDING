f79618b822f3b35b5efcbc5764d25ad6cd716f5a
shraddhaa09 <shraddhakhetmalis915@gmail.com>
2026-06-22 13:03:23 +0530

    solutions to the assignments 26

---
+++ assignments/assignment_26/2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void Pattern(int iRow,int iCol){
+    int i=0,j=0;
+    for(i=0;i<iRow;i++){
+        for(j=0;j<iCol;j++){
+            if(j<iCol-i){
+                printf("*\t");
+            }
+            else{
+                printf("#\t");
+            }    
+        }
+            printf("\n");
+        }        
+    }
+
+int main(){
+    int iValue1=0,iValue2=0;
+    printf("Enter number of rows:");
+    scanf("%d",&iValue1);
+    printf("Enter number of columns:");
+    scanf("%d",&iValue2);
+    Pattern(iValue1,iValue2);
+    return 0;
+}
