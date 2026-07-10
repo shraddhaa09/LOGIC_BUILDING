ad023e28890f1803510b3fc7143e8dda541824d1
shraddhaa09 <shraddhakhetmalis915@gmail.com>
2026-07-10 19:58:31 +0530

    Add program415 implementation

---
+++ DS/program415.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+#pragma pack(1)
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void Display(PNODE first,PNODE last){
+
+}
+int Count(PNODE first,PNODE last){
+    return 0;
+}
+void InsertFirst(PPNODE first,PPNODE last,int iNo){
+
+}
+void InsertLast(PPNODE first,PPNODE last,int iNo){
+
+}
+void InsertatPos(PPNODE first,PPNODE last,int iNo,int iPos){
+
+}
+void DeletFirst(PPNODE first,PPNODE last){
+
+}
+void DeletLast(PPNODE first,PPNODE last){
+
+}
+void DeletatPos(PPNODE first,PPNODE last,int iPos){
+
+}
+
+
+
+
+
+
+int main(){
+
+    PNODE head=NULL;
+    PNODE tail=NULL;
+
+
+
+    return 0;
+}
