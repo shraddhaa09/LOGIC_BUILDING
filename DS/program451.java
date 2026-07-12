0b7bf35d46f55e02dabeacaef86a25336d3bdd20
shraddhaa09 <shraddhakhetmalis915@gmail.com>
2026-07-12 12:48:46 +0530

    DS -> 12-07-2026

---
+++ DS/program451.java
@@ -0,0 +1,25 @@
+class node{
+    public int data;
+    public node next;
+
+    node(int no){
+        this.data=no;
+        this.next=null;
+    }
+}
+class SinglyLL{
+    public node first;
+    public int iCount;
+    public SinglyLL(){
+        System.out.println("inside the constructor\n");
+        this.first=null;
+        this.iCount=0;
+    }
+}
+class program451{
+    public static void main(String A[]){
+        SinglyLL sobj=new SinglyLL();
+
+        
+    }
+}
