49da653d56d57593d4c6f7551518f3b5ef5bfba8
shraddhaa09 <shraddhakhetmalis915@gmail.com>
2026-07-12 12:48:45 +0530

    DS -> 12-07-2026

---
+++ DS/program440.c
@@ -0,0 +1,231 @@
+#include<stdio.h>
+#include<stdlib.h>
+#pragma pack(1)
+
+struct node
+{
+    int data;
+    struct node *next;
+    struct node *prev;
+};
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void display(PNODE first,PNODE last){
+    if(first==NULL & last==NULL){
+        return;
+    }
+    printf("<=>");
+    do{
+        printf("|%d|<=>",first->data);
+        first=first->next;
+    }while(first!=last->next);
+    printf("\n");
+}
+int Count(PNODE first,PNODE last){
+    int iCount=0;
+    if(first==NULL & last==NULL){
+        return iCount;
+    }
+    do{
+        iCount++;
+        first=first->next;
+    }while(first!=last->next);
+    return iCount;
+}
+void insertFirst(PPNODE first,PPNODE last,int iNo){
+    PNODE newn=NULL;
+    newn=(PNODE)malloc(sizeof(NODE));
+
+    newn->data=iNo;
+    newn->next=NULL;
+    newn->prev=NULL;
+
+    if(*first==NULL && *last==NULL){
+        *first=newn;
+        *last=newn;
+    }
+    else{
+        newn->next=*first;
+        (*first)->prev=newn;
+        *first=newn;
+    }
+    (*last)->next=*first;
+    (*first)->prev=*last;
+
+}
+void insertLast(PPNODE first,PPNODE last,int iNo){
+    PNODE newn=NULL;
+    newn=(PNODE)malloc(sizeof(NODE));
+
+    newn->data=iNo;
+    newn->next=NULL;
+    newn->prev=NULL;
+
+    if(*first==NULL && *last==NULL){
+        *first=newn;
+        *last=newn;
+    }
+    else{
+        (*last)->next=newn;
+        newn->prev=*last;
+        *last=newn;
+        
+    }
+    (*last)->next=*first;
+    (*first)->prev=*last;
+
+}
+void insertatPos(PPNODE first,PPNODE last,int iNo,int iPos){
+    int i=0;
+    int iCount=Count(*first,*last);
+
+    PNODE newn=NULL;
+    PNODE temp=NULL;
+
+    if((iPos<1)||(iPos>(iCount+1))){
+        return;
+    }
+    if(iPos==1){
+        insertFirst(first,last,iNo);
+    }
+    else if(iPos==iCount+1){
+        insertLast(first,last,iNo);
+    }
+    else{
+        newn=(PNODE)malloc(sizeof(NODE));
+        newn->data=iNo;
+        newn->next=NULL;
+        newn->prev=NULL;
+
+        temp=*first;
+        for (i = 1; i < iPos-1; i++)
+        {
+            temp=temp->next;
+        }
+        newn->next=temp->next;
+        temp->next->prev=newn;
+
+        temp->next=newn;
+        newn->prev=temp;
+        
+    }
+
+
+}
+void DeleteFirst(PPNODE first,PPNODE last){
+    if(*first==NULL && *last==NULL){
+        return;
+    }
+    else if(*first==*last){
+        free(*first);
+        *first=NULL;
+        *last=NULL;
+
+    }
+    else{
+        *first=(*first)->next;
+        free((*first)->prev);
+
+        (*last)->next=*first;
+        (*first)->prev=*last;
+
+    }
+
+}
+void DeleteLast(PPNODE first,PPNODE last){
+    if(*first==NULL && *last==NULL){
+        return;
+    }
+    else if(*first==*last){
+        free(*first);
+        *first=NULL;
+        *last=NULL;
+
+    }
+    else{
+        *last=(*last)->prev;
+        free((*last)->next);
+
+        (*last)->next=*first;
+        (*first)->prev=*last;
+        
+
+    }
+
+}
+void DeleteatPost(PPNODE first,PPNODE last,int iPos){
+    int i=0;
+    int iCount=Count(*first,*last);
+    PNODE temp=NULL;
+
+    if((iPos<1)||(iPos>(iCount+1))){
+        return;
+    }
+    if(iPos==1){
+        DeleteFirst(first,last);
+    }
+    else if(iPos==iCount){
+        DeleteLast(first,last);
+    }
+    else{
+
+        temp=*first;
+        for (i = 1; i < iPos-1; i++)
+        {
+            temp=temp->next;
+        }
+        temp->next=temp->next->next;
+        free(temp->next->prev);
+        temp->next->prev=temp;
+    }
+
+}
+
+
+int main(){
+    PNODE head=NULL;
+    PNODE tail=NULL;
+    int iRet=0;
+
+    insertFirst(&head,&tail,51);
+    insertFirst(&head,&tail,21);
+    insertFirst(&head,&tail,11);
+
+    insertLast(&head,&tail,111);
+    insertLast(&head,&tail,101);
+    insertLast(&head,&tail,121);
+
+    display(head,tail);
+    iRet=Count(head,tail);
+    printf("the number of the nodes are: %d\n",iRet);
+
+    DeleteFirst(&head,&tail);
+
+    display(head,tail);
+    iRet=Count(head,tail);
+    printf("the number of the nodes are: %d\n",iRet);
+
+    DeleteLast(&head,&tail);
+
+    display(head,tail);
+    iRet=Count(head,tail);
+    printf("the number of the nodes are: %d\n",iRet);
+
+    insertatPos(&head,&tail,105,3);
+
+    display(head,tail);
+    iRet=Count(head,tail);
+    printf("the number of the nodes are: %d\n",iRet);
+
+    DeleteatPost(&head,&tail,4);
+    display(head,tail);
+    iRet=Count(head,tail);
+    printf("the number of the nodes are: %d\n",iRet);
+
+
+
+
+    return 0;
+}
