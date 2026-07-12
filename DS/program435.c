353743ea8d7a54d6c513b3ae4f3d0fa1fd460e40
shraddhaa09 <shraddhakhetmalis915@gmail.com>
2026-07-12 12:48:44 +0530

    DS -> 12-07-2026

---
+++ DS/program435.c
@@ -0,0 +1,101 @@
+#include<stdio.h>
+#include<stdlib.h>
+#pragma pack(1)
+
+struct node
+{
+    int data;
+    struct node *next;
+    struct node *prev;
+};
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void display(PNODE first,PNODE last){
+    if(first==NULL & last==NULL){
+        return;
+    }
+    printf("<=>");
+    do{
+        printf("|%d|<=>",first->data);
+        first=first->next;
+    }while(first!=last->next);
+    printf("\n");
+}
+int Count(NODE first,PNODE last){
+    return 0;
+}
+void insertFirst(PPNODE first,PPNODE last,int iNo){
+    PNODE newn=NULL;
+    newn=(PNODE)malloc(sizeof(NODE));
+
+    newn->data=iNo;
+    newn->next=NULL;
+    newn->prev=NULL;
+
+    if(*first==NULL && *last==NULL){
+        *first=newn;
+        *last=newn;
+    }
+    else{
+        newn->next=*first;
+        (*first)->prev=newn;
+        *first=newn;
+    }
+    (*last)->next=*first;
+    (*first)->prev=*last;
+
+}
+void insertLast(PPNODE first,PPNODE last,int iNo){
+    PNODE newn=NULL;
+    newn=(PNODE)malloc(sizeof(NODE));
+
+    newn->data=iNo;
+    newn->next=NULL;
+    newn->prev=NULL;
+
+    if(*first==NULL && *last==NULL){
+        *first=newn;
+        *last=newn;
+    }
+    else{
+        (*last)->next=newn;
+        newn->prev=*last;
+        *last=newn;
+        
+    }
+    (*last)->next=*first;
+    (*first)->prev=*last;
+
+}
+void insertatPos(PPNODE first,PPNODE last,int iNo,int iPos){
+
+}
+void DeleteFirst(PPNODE first,PPNODE last,int iNo){
+
+}
+void DeleteLast(PPNODE first,PPNODE last,int iNo){
+
+}
+void DeleteatPost(PPNODE first,PPNODE last,int iNo,int Ipos){
+
+}
+
+
+int main(){
+    PNODE head=NULL;
+    PNODE tail=NULL;
+
+    insertFirst(&head,&tail,51);
+    insertFirst(&head,&tail,21);
+    insertFirst(&head,&tail,11);
+
+    insertLast(&head,&tail,111);
+    insertLast(&head,&tail,101);
+    insertLast(&head,&tail,121);
+
+    display(head,tail);
+
+    return 0;
+}
